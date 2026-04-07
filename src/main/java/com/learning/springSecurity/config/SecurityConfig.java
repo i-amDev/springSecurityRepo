26287b1da276786a9bfa1f9ed0243288bec017e9
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-04-07 12:36:13 +0530

    - Created the "SecurityConfig" class to enable "Basic authentication" instead of the default "form-based" authentication. - Added the required properties to add a custom "username" and "password".

---
+++ src/main/java/com/learning/springSecurity/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.learning.springSecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+}
