91c56ba09f2042c5e325281461eaf96019f6ffc6
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-04-07 13:45:36 +0530

    - Created a bean of "CustomUserDetailService". - Created a bean of "PasswordEncoder". - Created a bean of "AuthenticationManager" with our "CustomUserDetailService" and "PasswordEncoder".

--- src/main/java/com/learning/springSecurity/config/SecurityConfig.java
+++ src/main/java/com/learning/springSecurity/config/SecurityConfig.java
@@ -1,10 +1,17 @@
 package com.learning.springSecurity.config;
 
+import com.learning.springSecurity.service.CustomUserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,8 +20,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
-        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated())
+        httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailService();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(daoAuthenticationProvider);
     }
 }
