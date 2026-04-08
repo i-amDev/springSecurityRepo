1b147c509705d48b0ff5530f87b2339c44af1ce7
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-04-08 11:32:16 +0530

    - Created one more user with role as "USER".

--- src/main/java/com/learning/springSecurity/service/AdminUserInitializer.java
+++ src/main/java/com/learning/springSecurity/service/AdminUserInitializer.java
@@ -1,5 +1,6 @@
 package com.learning.springSecurity.service;
 
+import com.learning.springSecurity.entity.Role;
 import com.learning.springSecurity.entity.Users;
 import com.learning.springSecurity.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -17,10 +18,20 @@ public class AdminUserInitializer {
                 Users admin = new Users();
                 admin.setUsername("admin");
                 admin.setPassword(passwordEncoder.encode("password"));
-                admin.setRole("ROLE_ADMIN");
+                admin.setRole(Role.ADMIN);
 
                 userRepository.save(admin);
                 System.out.println("Default admin user created!!");
+            }
+
+            if (userRepository.findByUsername("user").isEmpty()) {
+                Users user = new Users();
+                user.setUsername("user");
+                user.setPassword(passwordEncoder.encode("user"));
+                user.setRole(Role.USER);
+
+                userRepository.save(user);
+                System.out.println("Default user created!!");
             }
         };
     }
